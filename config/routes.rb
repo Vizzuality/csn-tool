80585a9e4b29245c939d3577fbac6519838c3d92
Simao Belchior <simaobelchior@gmail.com>
2016-10-10 23:59:09 +0100

    Adds species controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
   scope "(/:locale)", locale: /en|fr/ do
     get "/countries", to: "countries#index"
+    get "/species", to: "species#index"
   end
 
   get "/:locale", to: "home#index", as: :locale_root
