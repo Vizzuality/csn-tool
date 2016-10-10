562c357be08a331598125525751805e93f725300
Simao Belchior <simaobelchior@gmail.com>
2016-10-11 00:11:13 +0100

    Enables species index page

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -1,4 +1,9 @@
 class SpeciesController < ApplicationController
   def index
+    @species = if params[:query]
+                 Species.where("scientificname", params[:query])
+               else
+                 Species.all
+               end
   end
 end
---
+++ app/models/species.rb
@@ -0,0 +1,39 @@
+class Species < Carto
+  COLUMNS = [:scientificname, :red_list_status, :ssid, :commonname]
+
+  attr_reader *COLUMNS
+
+  def initialize(hsh)
+    COLUMNS.each do |col|
+      instance_variable_set("@#{col.to_s}", hsh[col.to_s])
+    end
+  end
+
+  private
+
+  def self.where_query field, value
+    %Q(
+      SELECT DISTINCT #{columns.join(", ")}
+      FROM #{table_name}
+      WHERE #{field} ilike '#{value}%'
+      ORDER BY #{order_column}
+    )
+  end
+
+  def self.list_query
+    %Q(
+      SELECT DISTINCT #{columns.join(", ")}
+      FROM #{table_name}
+      WHERE scientificname IS NOT NULL
+      ORDER BY #{order_column}
+    )
+  end
+
+  def self.order_column
+    "scientificname"
+  end
+
+  def self.table_name
+    "species_and_flywaygroups"
+  end
+end
