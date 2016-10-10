b501db565a0980c558f4fb373b1dbdbd94219bba
Simao Belchior <simaobelchior@gmail.com>
2016-10-10 22:44:42 +0100

    Adds countries controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   scope "(/:locale)", locale: /en|fr/ do
+    get "/countries", to: "countries#index"
   end
 
   get "/:locale", to: "home#index", as: :locale_root
