e31cb83a220231d2cf53e486ec9f390bc097f851
Simao Belchior <simaobelchior@gmail.com>
2016-10-10 18:51:05 +0100

    Starting up I18n

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module CsnTool
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.enforce_available_locales = false
+    config.i18n.default_locale = :en
   end
 end
