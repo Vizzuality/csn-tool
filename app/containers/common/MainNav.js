b0094897e5aeff3167b353c092b015b4f1e0c65a
j8seangel <jseangel@gmail.com>
2016-11-10 18:07:50 +0100

    Mark as active the current page

--- app/containers/common/MainNav.js
+++ app/containers/common/MainNav.js
@@ -10,4 +10,7 @@ const mapDispatchToProps = (dispatch) => ({
   updateLang: lang => dispatch(updateLang(lang))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(MainNav);
+// {pure: false} documentation
+// https://github.com/reactjs/react-redux/blob/master/docs/troubleshooting.md#my-views-arent-updating-when-something-changes-outside-of-redux
+// to update the activeClassName in router changes.
+export default connect(mapStateToProps, mapDispatchToProps, null, { pure: false })(MainNav);
