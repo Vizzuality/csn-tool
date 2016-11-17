ce2fa43868cf24a58cdb557ad28f5f863a9c0ef6
j8seangel <jseangel@gmail.com>
2016-11-17 17:28:15 +0100

    County sites and species endpoints

--- api/controllers/countries.js
+++ api/controllers/countries.js
@@ -20,10 +20,7 @@ function getCountries(req, res) {
 }
 
 function getCountry(req, res) {
-  const query = `SELECT c.country, c.iso3, si.hyperlink, si.protection_status, si.site_name, si.lat, si.lon, si.site_id
-    FROM countries c
-    INNER JOIN sites si ON si.country_id = c.country_id
-    WHERE c.iso3='${req.params.iso}'`;
+  const query = `SELECT * FROM countries WHERE iso3='${req.params.iso}'`;
   rp(CARTO_SQL + query)
     .then((data) => {
       const result = JSON.parse(data);
@@ -40,7 +37,63 @@ function getCountry(req, res) {
     });
 }
 
+function getCountrySites(req, res) {
+  const query = `with stc as (select site_id, SUM(case when csn_criteria = '' then 0 else 1 end) as csn, SUM(case when iba_criteria = '' then 0 else 1 end) as iba  from species_sites group by site_id)
+  SELECT c.country, c.iso3,
+    s.protection_status, s.site_name, s.lat, s.lon,
+    stc.csn, stc.iba
+  FROM sites s
+	INNER JOIN countries c ON s.country_id = c.country_id and c.iso3 = '${req.params.iso}'
+	INNER JOIN stc ON stc.site_id = s.site_id`;
+  rp(CARTO_SQL + query)
+    .then((data) => {
+      const result = JSON.parse(data);
+      if (result.rows && result.rows.length > 0) {
+        res.json(result.rows);
+      } else {
+        res.status(404);
+        res.json({ error: 'No sites found' });
+      }
+    })
+    .catch((err) => {
+      res.status(err.statusCode || 500);
+      res.json({ error: err.message });
+    });
+}
+
+function getCountrySpecies(req, res) {
+  const query = `SELECT c.country, c.iso3, sp.english_name as species, sp.genus, sp.family, ps.populations
+    FROM countries c
+    INNER JOIN sites s ON s.country_id = c.country_id
+    INNER JOIN species_country sc ON sc.country_id = s.country_id
+	  INNER JOIN species sp ON sp.species_id = sc.species_id
+    INNER JOIN populations_species_no_geo ps ON ps.sisrecid = sc.species_id
+    WHERE c.iso3='${req.params.iso}'
+    GROUP BY c.country, c.iso3, sp.english_name, sp.genus, sp.family, ps.populations`;
+  rp(CARTO_SQL + query)
+    .then((data) => {
+      const result = JSON.parse(data);
+      if (result.rows && result.rows.length > 0) {
+        res.json(result.rows);
+      } else {
+        res.status(404);
+        res.json({ error: 'No species found' });
+      }
+    })
+    .catch((err) => {
+      res.status(err.statusCode || 500);
+      res.json({ error: err.message });
+    });
+}
+
+function getCountryPopulations(req, res) {
+  res.json({ msg: 'Comming soon' });
+}
+
 module.exports = {
   getCountries,
-  getCountry
+  getCountry,
+  getCountrySites,
+  getCountrySpecies,
+  getCountryPopulations
 };
--- api/routes.js
+++ api/routes.js
@@ -7,6 +7,9 @@ const router = Express.Router(); // eslint-disable-line new-cap
 
 router.route('/countries').get(CountriesCtrl.getCountries);
 router.route('/countries/:iso').get(CountriesCtrl.getCountry);
+router.route('/countries/:iso/sites').get(CountriesCtrl.getCountrySites);
+router.route('/countries/:iso/species').get(CountriesCtrl.getCountrySpecies);
+router.route('/countries/:iso/populations').get(CountriesCtrl.getCountryPopulations);
 router.route('/sites').get(SitesCtrl.getSites);
 router.route('/species').get(SpeciesCtrl.getSpeciesList);
 router.route('/species/:slug').get(SpeciesCtrl.getSpecies);
