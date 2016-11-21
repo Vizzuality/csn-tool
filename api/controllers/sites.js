3eacd713c0acd0c9d0819427599aaef801f6f0c6
j8seangel <jseangel@gmail.com>
2016-11-21 19:05:07 +0100

    Add api endpoint to sites detail

--- api/controllers/sites.js
+++ api/controllers/sites.js
@@ -2,7 +2,28 @@ const rp = require('request-promise');
 const CARTO_SQL = require('../constants').CARTO_SQL;
 
 function getSites(req, res) {
-  const query = 'SELECT * FROM sites LIMIT 50';
+  const query = 'with p as (SELECT DISTINCT site_id FROM species_sites) SELECT * FROM sites WHERE site_id in (SELECT * from p) LIMIT 100';
+  rp(CARTO_SQL + query)
+    .then((data) => {
+      const result = JSON.parse(data);
+      if (result.rows && result.rows.length > 0) {
+        res.json(result.rows);
+      } else {
+        res.status(404);
+        res.json({ error: 'No sites' });
+      }
+    })
+    .catch((err) => {
+      res.status(err.statusCode || 500);
+      res.json({ error: err.message });
+    });
+}
+
+function getSitesDetail(req, res) {
+  const query = `SELECT s.*, ss.csn_criteria, ss.iba_criteria, ss.season, si.lat, si.lon, si.site_name
+    FROM species s
+    INNER JOIN species_sites ss ON ss.species_id = s.species_id
+    INNER JOIN sites si ON si.site_id = ss.site_id AND si.slug = '${req.params.slug}'`;
   rp(CARTO_SQL + query)
     .then((data) => {
       const result = JSON.parse(data);
@@ -20,5 +41,6 @@ function getSites(req, res) {
 }
 
 module.exports = {
-  getSites
+  getSites,
+  getSitesDetail
 };
