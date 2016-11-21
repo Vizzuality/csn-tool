ff4b2c7f042cf7d77d87459c7bb4532ff7bcf96a
Clara Linos <clara.linos@gmail.com>
2016-11-21 17:17:01 +0100

    use cases module styles

--- app/components/pages/HomePage.js
+++ app/components/pages/HomePage.js
@@ -6,7 +6,8 @@ function HomePage() {
     <div className="l-page">
       <Banner />
       <div className="l-content row">
-        <div className="column small-12 medium-6 medium-offset-3">
+        <div className="column small-12 medium-10 medium-offset-1">
+          <h2 className="header -main -center">What can I use this tool for?</h2>
           <ul className="c-use-cases">
             <li className="use">
               <svg>
@@ -27,9 +28,11 @@ function HomePage() {
               <h3 className="header -intro">You can also search for a species and see its critical sites</h3>
             </li>
           </ul>
+        </div>
+        <div className="column small-12 medium-6 medium-offset-3">
           <p className="text -intro">The tool makes it easy to obtain information on the sites critical for waterbird species by accessing several independent databases and analysing information at the biogeographical population level, so providing a comprehensive basis for management and decision making. It is designed to help a range of different users from site managers to national authorities and international organisations. The CSN Tool, one of the major achievements of the Wings Over Wetlands (WOW) project, is also an important example of the added value of cooperation between likeminded conservation organisations, international conventions and agreements, governments, UN agencies and other donors.</p>
 
-          <p>The CSN Tool supports both AEWA and the Ramsar Convention on Wetlands. It is also relevant to the EU Birds Directive and the Bern Convention’s Emerald Network.</p>
+          <p className="text -intro">The CSN Tool supports both AEWA and the Ramsar Convention on Wetlands. It is also relevant to the EU Birds Directive and the Bern Convention’s Emerald Network.</p>
           </div>
       </div>
     </div>
