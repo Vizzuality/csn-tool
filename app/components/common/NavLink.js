7d0ad866ddfef5b1a428fc28e430132859f619b6
Clara Linos <clara.linos@gmail.com>
2016-11-22 17:12:39 +0100

    lint

--- app/components/common/NavLink.js
+++ app/components/common/NavLink.js
@@ -26,7 +26,7 @@ NavLink.propTypes = {
   text: React.PropTypes.string,
   // Define the text to show translated
   i18nText: React.PropTypes.string,
-  //Define the icon used for the link
+  // Define the icon used for the link
   icon: React.PropTypes.string
 };
 
