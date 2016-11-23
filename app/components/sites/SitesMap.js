72ddadfe9224f0495f6d1aab20bbbc81c685006f
Clara Linos <clara.linos@gmail.com>
2016-11-23 17:35:54 +0100

    new basemap

--- app/components/sites/SitesMap.js
+++ app/components/sites/SitesMap.js
@@ -14,7 +14,7 @@ class SitesMap extends React.Component {
     this.map.attributionControl.addAttribution('&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>');
     this.map.zoomControl.setPosition('topright');
     this.map.scrollWheelZoom.disable();
-    this.tileLayer = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png').addTo(this.map).setZIndex(0);
+    this.tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/csn/civtok4xx004d2kpo3acytide/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY3NuIiwiYSI6ImNpdnRvam1qeDAwMXgyenRlZjZiZWc1a2wifQ.Gr5pLJzG-1tucwY4h-rGdA').addTo(this.map).setZIndex(0);
 
     if (this.props.data && this.props.data.length) {
       this.drawMarkers(this.props.data);
