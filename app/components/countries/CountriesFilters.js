df674a1cc6b4f0c2e449065ee11b72334b9bec5c
Clara Linos <clara.linos@gmail.com>
2016-11-23 16:44:22 +0100

    class for active state on tabs

--- app/components/countries/CountriesFilters.js
+++ app/components/countries/CountriesFilters.js
@@ -2,13 +2,14 @@ import React from 'react';
 import NavLink from 'containers/common/NavLink';
 
 function TableFilters(props, context) {
+
   return (
     <div className="row c-table-filters">
       <div className="column small-12 medium-5">
         <div className="tags">
-          <NavLink to={`/countries/${props.country}/sites`} i18nText="sites" />
-          <NavLink to={`/countries/${props.country}/species`} i18nText="species" />
-          <NavLink to={`/countries/${props.country}/populations`} i18nText="populations" />
+          <NavLink to={`/countries/${props.country}/sites`} i18nText="sites" className={props.category && props.category === "sites" ? "is-active" : ""}/>
+          <NavLink to={`/countries/${props.country}/species`} i18nText="species" className={props.category && props.category === "species" ? "is-active" : ""}/>
+          <NavLink to={`/countries/${props.country}/populations`} i18nText="populations" className={props.category && props.category === "populations" ? "is-active" : ""}/>
         </div>
       </div>
       <div className="column small-12 offset-medium-1 medium-2 filters">
