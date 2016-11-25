f4aed4ed426796ccb34e196189ce904f54dc8083
Gerardo <gerardo.pacheco@vizzuality.com>
2016-11-25 11:31:22 +0100

    Feat: Api - Species - New routes

--- api/routes.js
+++ api/routes.js
@@ -5,14 +5,22 @@ const SpeciesCtrl = require('./controllers/species');
 
 const router = Express.Router(); // eslint-disable-line new-cap
 
+// Countries
 router.route('/countries').get(CountriesCtrl.getCountries);
 router.route('/countries/:iso').get(CountriesCtrl.getCountry);
 router.route('/countries/:iso/sites').get(CountriesCtrl.getCountrySites);
 router.route('/countries/:iso/species').get(CountriesCtrl.getCountrySpecies);
 router.route('/countries/:iso/populations').get(CountriesCtrl.getCountryPopulations);
+
+// Sites
 router.route('/sites').get(SitesCtrl.getSites);
 router.route('/sites/:slug').get(SitesCtrl.getSitesDetail);
+
+// Species
 router.route('/species').get(SpeciesCtrl.getSpeciesList);
-router.route('/species/:slug').get(SpeciesCtrl.getSpecies);
+router.route('/species/:slug').get(SpeciesCtrl.getSpeciesSites);
+router.route('/species/:slug/population').get(SpeciesCtrl.getSpeciesPopulation);
+router.route('/species/:slug/threats').get(SpeciesCtrl.getSpeciesSites);
+router.route('/species/:slug/habitats').get(SpeciesCtrl.getSpeciesSites);
 
 module.exports = router;
