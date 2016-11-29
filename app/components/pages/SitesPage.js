4ba2d5bd91c0cee137e408ad54c76c96ee59543d
Clara Linos <clara.linos@gmail.com>
2016-11-29 12:57:40 +0100

    species review

--- app/components/pages/SitesPage.js
+++ app/components/pages/SitesPage.js
@@ -24,40 +24,58 @@ class SitesPage extends React.Component {
 
   render() {
     return (
-      <div className={`l-page ${this.props.viewMode}`}>
+      <div className="l-page">
         <div className="l-navigation">
           <div className="row">
             <div className="column">
-              <div className="navigation-wrapper">
-                {this.props.selected &&
+              {this.props.selected ?
+                <div className="navigation-wrapper">
                   <div className="c-navigation">
                     <NavLink className="breadcrumb" to="/sites" i18nText="backToSites" />
                     <h2>{unslug(this.props.selected)}</h2>
                   </div>
-                }
-                <ViewToggler
-                  viewMode={this.props.viewMode}
-                  setViewMode={this.props.setViewMode}
-                />
-              </div>
+                </div>
+              : <div className="navigation-wrapper">
+                  <div className="c-navigation">
+                    <h2>Sites</h2>
+                  </div>
+                  <ViewToggler
+                    viewMode={this.props.viewMode}
+                    setViewMode={this.props.setViewMode}
+                  />
+                </div>
+              }
             </div>
           </div>
         </div>
+        {this.props.selected ?
+          <div className="l-page">
+            <div className="l-map -header -short">
+              <SitesMap slug={this.props.selected} />
+            </div>
 
-        <div className={`l-mask ${this.props.viewMode}`}>
-          <div className="l-map -species-detail">
-            <SitesMap slug={this.props.selected} />
-          </div>
-
-          <div className="l-table">
-            <div className="row">
-              <div className="column">
-                <SitesTable data={this.props.list} slug={this.props.selected} category={this.props.category} />
+            <div className="l-table">
+              <div className="row">
+                <div className="column">
+                  <SitesTable data={this.props.list} slug={this.props.selected} category={this.props.category} />
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        : <div className={`l-mask ${this.props.viewMode}`}>
+            <div className="l-map -header">
+              <SitesMap slug={this.props.selected} />
+            </div>
 
+            <div className="l-table">
+              <div className="row">
+                <div className="column">
+                  <SitesTable data={this.props.list} slug={this.props.selected} category={this.props.category} />
+                </div>
+              </div>
+            </div>
+          </div>
+        }
       </div>
     );
   }
