96b926c2bd82de75a3796aac5a49ebccf82023b8
j8seangel <jseangel@gmail.com>
2016-12-07 11:08:21 +0100

    Add translations and home intro

--- app/components/common/ViewToggler.js
+++ app/components/common/ViewToggler.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
-function ViewToggler(props) {
+function ViewToggler(props, context) {
   return (
     <ul className="c-view-toggler">
-      <li className={props.viewMode === 'map' ? 'is-active toggler' : 'toggler'} onClick={() => props.setViewMode('map')} >Map</li>
-      <li className={props.viewMode === 'list' ? 'is-active toggler' : 'toggler'} onClick={() => props.setViewMode('list')} >Table</li>
+      <li
+        className={props.viewMode === 'map' ? 'is-active toggler' : 'toggler'}
+        onClick={() => props.setViewMode('map')}
+      >
+        {context.t('map')}
+      </li>
+      <li
+        className={props.viewMode === 'list' ? 'is-active toggler' : 'toggler'}
+        onClick={() => props.setViewMode('list')}
+      >
+        {context.t('table')}
+      </li>
     </ul>
   );
 }
+
+ViewToggler.contextTypes = {
+  // Define function to get the translations
+  t: React.PropTypes.func.isRequired
+};
 
 ViewToggler.propTypes = {
   viewMode: React.PropTypes.string,
