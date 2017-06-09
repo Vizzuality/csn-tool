9937fc86f0a9d8c7cb1190fb48557ed7904c5b5c
utilitymonster <mrcordell@gmail.com>
2017-06-09 09:59:43 +0200

    You've been interpunct

--- app/components/tables/TableListHeader.js
+++ app/components/tables/TableListHeader.js
@@ -52,7 +52,7 @@ class HeaderFilter {
 
 function getFilters(columns, data) {
   const filters = {};
-  const INDENT = ' > ';
+  const INDENT = '· ';
   if (columns && columns.length) {
     columns.forEach((column) => {
       const columnFilter = columnsWithFilter.filter((col) => col.column === column)[0];
