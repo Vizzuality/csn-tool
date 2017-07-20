e429fe2c6fa43e9608722ae88ad0afb1d62e6657
Simao Belchior <simaobelchior@gmail.com>
2017-07-20 08:58:32 +0100

    Avoid errors if there's no protection status. fill slug for critical sites

--- api/helpers/index.js
+++ api/helpers/index.js
@@ -1,9 +1,11 @@
 function normalizeSiteStatus(string) {
-  const uString = string.toUpperCase();
-  if (uString.indexOf('WHOLE') >= 0) return 'whole';
-  if (uString.indexOf('MOST') >= 0) return 'most';
-  if (uString.indexOf('SOME') >= 0) return 'some';
-  if (uString.indexOf('LITTLE') >= 0) return 'little';
+  if (string && string !== undefined) {
+    const uString = string.toUpperCase();
+    if (uString.indexOf('WHOLE') >= 0) return 'whole';
+    if (uString.indexOf('MOST') >= 0) return 'most';
+    if (uString.indexOf('SOME') >= 0) return 'some';
+    if (uString.indexOf('LITTLE') >= 0) return 'little';
+  }
   return 'unknown';
 }
 
