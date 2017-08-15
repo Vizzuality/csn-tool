e8a41eaa67b2266a0eb5467ce6cb99be6ca19556
Simao Belchior <simaobelchior@gmail.com>
2017-08-15 09:10:59 +0100

    Enables downloading threshold data

--- app/components/threshold/ThresholdFilters.js
+++ app/components/threshold/ThresholdFilters.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import CSVButton from 'components/tables/CSVButton.js';
 import ThresholdSearch from 'containers/threshold/ThresholdSearch';
 
-function TresholdFilters() {
+function TresholdFilters(props) {
   return (
     <div className="row c-table-filters">
-      <div className="column small-12 medium-4 medium-offset-8">
+      <div className="column small-12 medium-8">
+        <h2>Matching Populations
+          <CSVButton data={props.data} columns={props.columns} />
+        </h2>
+      </div>
+      <div className="column small-12 medium-4">
         <ThresholdSearch placeholder="sitesFilter" />
       </div>
     </div>
   );
 }
 
-TresholdFilters.propTypes = {};
+TresholdFilters.propTypes = {
+  data: React.PropTypes.array.isRequired,
+  columns: React.PropTypes.array.isRequired
+};
 
 export default TresholdFilters;
