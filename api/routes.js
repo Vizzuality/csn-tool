5de16250384c592c77d77744c946ead7ddab2e4c
dancer2090 <ivaneichyk.ivan@gmail.com>
2020-01-20 18:08:46 +0200

    created development api route

--- api/routes.js
+++ api/routes.js
@@ -15,6 +15,7 @@ router.route('/countries/:iso/criticalSites').get(CountriesCtrl.getCountryCritic
 router.route('/countries/:iso/species').get(CountriesCtrl.getCountrySpecies);
 router.route('/countries/:iso/populations').get(CountriesCtrl.getCountryPopulations);
 router.route('/countries/:iso/look-alike-species').get(CountriesCtrl.getCountryPopsWithLookAlikeCounts);
+router.route('/countries/:iso/look-alike-species-new').get(CountriesCtrl.getCountryPopsWithLookAlikeCountsSecond);
 router.route('/countries/:iso/look-alike-species/:populationId').get(CountriesCtrl.getCountryLookAlikeSpecies);
 
 // Sites
