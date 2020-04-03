387ff3df7377adfab3e1f81ee97394f369f96711
dancer2090 <ivaneichyk.ivan@gmail.com>
2020-04-03 13:10:34 +0300

    climate layers switch logic - code fix

--- app/actions/common.js
+++ app/actions/common.js
@@ -40,21 +40,13 @@ export function commonToggleClimateLayers(items, itemActive, toggleLayer) {
     if (items.length > 0) {
       dispatch(toggleLayer(itemActive.layer));
       const season = itemActive.layer.split('_')[2];
-      if (itemActive.layer.indexOf('climate_gains') !== -1) {
-        items.filter(item =>
-          item.layer !== itemActive.layer
-          && item.active
-          && item.layer.split('_')[2] !== season
-        )
-          .forEach(item => dispatch(toggleLayer(item.layer)));
-      } else {
-        items.filter(item =>
-          item.layer !== itemActive.layer
-          && item.active
-          && item.layer.split('_')[2] !== season
-        )
-          .forEach(item => dispatch(toggleLayer(item.layer)));
-
+      items.filter(item =>
+        item.layer !== itemActive.layer
+        && item.active
+        && item.layer.split('_')[2] !== season
+      )
+        .forEach(item => dispatch(toggleLayer(item.layer)));
+      if (itemActive.layer.indexOf('climate_gains') === -1) {
         items.filter(item =>
           item.layer.indexOf('climate_gains') === -1
           & item.active
